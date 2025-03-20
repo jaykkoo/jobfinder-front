69a831e0ece6fa947a53951088a41a80bd88bd41
jaykkoo <agutleben@outlook.fr>
2025-03-20 15:56:55 +0100

    add csrftoken

--- src/api/AxiosConfig.jsx
+++ src/api/AxiosConfig.jsx
@@ -4,5 +4,12 @@ const axiosInstance = axios.create({
   baseURL: "http://35.180.198.48",
   withCredentials: true,
 });
+axiosInstance.interceptors.request.use((config) => {
+  const csrfToken = Cookies.get('csrftoken');  // Get CSRF token from cookies
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;  // Set CSRF token in request header
+  }
+  return config;
+});
 
 export default axiosInstance;
